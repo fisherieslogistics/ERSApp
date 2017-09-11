dee6514cc44bea13da94327645469564178b0320
fllfl <rimu@fisherieslogistics.com>
2017-09-12 10:06:15 +1200

    use production url

--- app/constants/ServerURL.js
+++ app/constants/ServerURL.js
@@ -1,4 +1,5 @@
-export default 'http://localhost:8000/'
+//export default 'http://localhost:8000/'
 //export default 'http://192.168.0.101:8000/'
 //export default 'https://test.catchhub.com/'
 //export default 'http://192.168.1.239:8000/'
+export default 'https://catchhub.com/'
