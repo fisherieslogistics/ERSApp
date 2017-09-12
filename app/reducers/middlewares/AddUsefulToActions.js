9f95c94c59dedd06b03f109405346395022b2d19
fllfl <rimu@fisherieslogistics.com>
2017-09-13 08:19:27 +1200

    remove the version from each and every action --

--- app/reducers/middlewares/AddUsefulToActions.js
+++ app/reducers/middlewares/AddUsefulToActions.js
@@ -1,13 +1,10 @@
 import moment from 'moment';
-import VERSION from '../../constants/version';
 
 export default function AddUsefulToActions() {
-
 
   return (next) => (action) => next(Object.assign({}, action, {
     payload: {
       ...action.payload,
       timestamp: moment().toISOString(),
-      RAVersion: VERSION,
-    }}))
+    }}));
 }
